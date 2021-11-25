e2c7b84bff50086e37097f41d0b4a9999300502c
Sylvie-B <unsolo@hotmail.fr>
2021-11-25 10:51:43 +0100

    review case[i] to this

--- appGame.js
+++ appGame.js
@@ -13,35 +13,37 @@ let point = document.getElementsByClassName('point');
 
 let turn = 0;   // player 1 : X
 let count = 0;
+
 for(let i = 0 ; i < cases.length ; i++){                 // for each cases
     cases[i].addEventListener('mouseup', function (event){   // on click
-        if(count < 8){
-            if(cases[i].innerHTML.length === 0){         // if case is empty
+        if(count < 9){
+            if(this.innerHTML.length === 0){         // if case is empty
+                ++count;
                 switch (event.button){
                     case 0 :
                         if(turn === 0){
-                            cases[i].innerHTML = "X";        // write player mark
-                            cases[i].style.color = "crimson";
+                            this.innerHTML = "X";        // write player mark
+                            this.style.color = "crimson";
                             checkGame("X", "crimson");             // verify if player win
                             gameBoard.style.cursor = 'url("playerO.png"), auto'
                             turn++;                         // next player
-                            count++;
                         }
                         break;
                     case 2 :
                         if(turn === 1){
-                            cases[i].innerHTML = "O";
-                            cases[i].style.color = "dodgerblue";
+                            this.innerHTML = "O";
+                            this.style.color = "dodgerblue";
                             checkGame("O", "dodgerblue");
                             gameBoard.style.cursor = 'url("playerX.png"), auto'
                             turn--;
-                            count++;
                         }
                         break;
                 }
             }
         }
         else {
+            checkGame("X", "crimson");
+            checkGame("O", "dodgerblue");
             result.style.display = "flex";
             XoO.style.backgroundColor = "black";
             XoO.innerHTML = "Partie nulle";
@@ -50,6 +52,7 @@ for(let i = 0 ; i < cases.length ; i++){                 // for each cases
     })
 }
 
+// restart game
 restart.addEventListener('click', function (){  // click restart
     gameBoard.style.cursor = 'url("playerX.png"), auto'
     for (let i = 0 ; i < cases.length ; i++){
@@ -60,6 +63,7 @@ restart.addEventListener('click', function (){  // click restart
     turn = count = 0;
 })
 
+// remove result display to continue the game
 result.addEventListener('click', function (){
     result.style.display = 'none';
     for (let i = 0 ; i < cases.length ; i++){
@@ -68,6 +72,7 @@ result.addEventListener('click', function (){
     count = 0;
 })
 
+// verify if therre
 function checkGame (player, color) {
     if(checkDiagonal(player) || checkHorizontal(player) || checkVertical(player)){
         XoO.innerHTML = "JOUEUR <span>" + player + "</span> GAGNE";
